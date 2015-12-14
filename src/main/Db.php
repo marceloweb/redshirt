97d56b1c2c8d643d9734228655a9b7f04e1ec4ac
Marcelo Oliveira <marcelo@marceloweb.info>
2015-12-14 00:29:03 -0200

    - access data

--- src/main/Db.php
+++ src/main/Db.php
@@ -5,7 +5,7 @@ class Db {
     private $connection;
 
     function __construct() {
-        $this->connection = new PDO("mysql:host=localhost;dbname=redshirt;charset=utf8", "root", "ammy0709");
+        $this->connection = new PDO("mysql:host=localhost;dbname=redshirt;charset=utf8", "", "");
     }
 
     public function getInstance() {
